8ae1b4a15e7884e023adbb06c33c4aaede033f2c
BenjaminBrehier <benbrehier@gmail.com>
2025-01-08 11:44:44 +0100

    feat: details screen and move api call to launch

--- app/src/main/java/com/bl/rustyze/ui/components/VehicleCard.kt
+++ app/src/main/java/com/bl/rustyze/ui/components/VehicleCard.kt
@@ -1,6 +1,7 @@
 package com.bl.rustyze.ui.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -15,13 +16,14 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
 
 @Composable
 fun VehicleCard(
     vehicleName: String,
     rustyMeter: String,
     iconColor: Color = MaterialTheme.colorScheme.primary,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
 ) {
     Card(
         modifier = modifier
