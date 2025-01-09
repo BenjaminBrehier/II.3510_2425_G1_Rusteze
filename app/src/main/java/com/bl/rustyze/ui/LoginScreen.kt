7802ef6d184c5306308b0c6853c0e6c92f939f1e
BenjaminBrehier <benbrehier@gmail.com>
2025-01-09 18:30:25 +0100

    feat: login with X + style login google

--- app/src/main/java/com/bl/rustyze/ui/LoginScreen.kt
+++ app/src/main/java/com/bl/rustyze/ui/LoginScreen.kt
@@ -1,16 +1,30 @@
 package com.bl.rustyze.ui
 
+import android.app.Activity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import com.bl.rustyze.R
 import com.google.android.gms.auth.api.signin.GoogleSignIn
@@ -19,6 +33,7 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.OAuthProvider
 
 @Composable
 fun LoginScreen(
@@ -30,11 +45,11 @@ fun LoginScreen(
     var password by remember { mutableStateOf("") }
     var isLogin by remember { mutableStateOf(true) } // Toggle between login and register
     val context = LocalContext.current
+    val auth = FirebaseAuth.getInstance()
 
     // Fonction pour connecter avec Firebase en utilisant le compte Google
-    fun firebaseAuthWithGoogle(account: GoogleSignInAccount, onAuthSuccess: () -> Unit, onAuthFailure: (String) -> Unit) {
+    fun firebaseAuthWithGoogle(account: GoogleSignInAccount) {
         val credential = GoogleAuthProvider.getCredential(account.idToken, null)
-        val auth = FirebaseAuth.getInstance()
         auth.signInWithCredential(credential)
             .addOnCompleteListener { task ->
                 if (task.isSuccessful) {
@@ -51,13 +66,12 @@ fun LoginScreen(
             val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
             try {
                 val account = task.getResult(ApiException::class.java)
-                firebaseAuthWithGoogle(account, onAuthSuccess, onAuthFailure)
+                firebaseAuthWithGoogle(account)
             } catch (e: ApiException) {
                 onAuthFailure("Google sign-in failed: ${e.message}")
             }
         }
     )
-
 
     fun startGoogleSignIn() {
         val googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
@@ -67,6 +81,29 @@ fun LoginScreen(
 
         val googleSignInClient = GoogleSignIn.getClient(context, googleSignInOptions)
         googleSignInLauncher.launch(googleSignInClient.signInIntent)
+    }
+
+    // Fonction pour connecter avec Firebase en utilisant Twitter (X)
+    fun startTwitterSignIn() {
+        val provider = OAuthProvider.newBuilder("twitter.com")
+        val pendingResultTask = auth.pendingAuthResult
+        if (pendingResultTask != null) {
+            pendingResultTask
+                .addOnSuccessListener { result ->
+                    onAuthSuccess()
+                }
+                .addOnFailureListener { exception ->
+                    onAuthFailure("Twitter authentication failed: ${exception.message}")
+                }
+        } else {
+            auth.startActivityForSignInWithProvider(context as Activity, provider.build())
+                .addOnSuccessListener { result ->
+                    onAuthSuccess()
+                }
+                .addOnFailureListener { exception ->
+                    onAuthFailure("Twitter authentication failed: ${exception.message}")
+                }
+        }
     }
 
     Column(
@@ -105,7 +142,6 @@ fun LoginScreen(
 
         Button(
             onClick = {
-                val auth = FirebaseAuth.getInstance()
                 if (isLogin) {
                     // Login with email/password
                     auth.signInWithEmailAndPassword(email, password)
@@ -144,16 +180,55 @@ fun LoginScreen(
             )
         }
 
-        Spacer(modifier = Modifier.height(8.dp))
+        Spacer(modifier = Modifier.height(16.dp))
 
-        AndroidView(
-            factory = { context ->
-                com.google.android.gms.common.SignInButton(context).apply {
-                    setSize(com.google.android.gms.common.SignInButton.SIZE_WIDE)
-                    setOnClickListener { startGoogleSignIn() }
-                }
-            },
-            modifier = Modifier.fillMaxWidth()
+        SocialLoginButton(
+            text = "Login with Google",
+            backgroundColor = Color(0xFF3F85F4),
+            icon = ImageVector.vectorResource(id = R.drawable.ic_google),
+            onClick = { startGoogleSignIn() }
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        SocialLoginButton(
+            text = "Login with X",
+            backgroundColor = Color.Black,
+            icon = ImageVector.vectorResource(id = R.drawable.ic_x),
+            onClick = { startTwitterSignIn()}
+        )
+    }
+}
+
+@Composable
+fun SocialLoginButton(
+    text: String,
+    backgroundColor: Color,
+    icon: ImageVector,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+            .fillMaxWidth()
+            .height(50.dp)
+            .background(color = backgroundColor, shape = RoundedCornerShape(5.dp))
+            .clickable(onClick = onClick)
+            .padding(horizontal = 12.dp)
+    ) {
+        Icon(
+            imageVector = icon,
+            contentDescription = null,
+            tint = Color.White,
+            modifier = Modifier.size(24.dp)
+        )
+        Spacer(modifier = Modifier.width(12.dp))
+        Text(
+            text = text,
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.White
         )
     }
 }
